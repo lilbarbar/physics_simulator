6283446abbfe86c0fa90376180a8a8057f8b48b5
abubahmed <ubuntu@ip-172-31-22-188.us-east-2.compute.internal>
2025-07-30 20:45:03 +0000

    .

--- lib/click_interactions.ml
+++ lib/click_interactions.ml
@@ -5,37 +5,64 @@ open! Graphics
 let on_button_click (t : World.t) id =
   match id with
   | "create-ball-btn" ->
-    t.click_state <- Click_state.Create_object_now Objects.Ball
+    t.click_state <- Click_state.Create_object_select_first Objects.Ball
   | "create-line-btn" ->
-    t.click_state <- Click_state.Create_object_now Objects.Line
+    t.click_state <- Click_state.Create_object_select_first Objects.Line
   | "create-cup-btn" ->
-    t.click_state <- Click_state.Create_object_now Objects.Cup
+    t.click_state <- Click_state.Create_object_select_first Objects.Cup
   | _ -> ()
 ;;
 
-let handle_create_object (t : World.t) (obj : Objects.t) x y =
+let handle_select_object_first (t : World.t) (obj : Objects.t) x y =
   print_endline "handle_create_object";
-  let create_at = { Vector.x = Float.of_int x; y = Float.of_int y } in
-  let create_at_max = Vector.( + ) { Vector.x = 30.0; y = 30.0 } create_at in
+  let first_selected_pos =
+    { Vector.x = Float.of_int x; y = Float.of_int y }
+  in
   if Interface.Canvas.in_bounds t.ui.canvas x y
   then (
     match obj with
-    | Ball ->
+    | Objects.Ball ->
+      t.click_state
+      <- Click_state.Create_object_select_final
+           (Objects.Ball, first_selected_pos)
+    | Objects.Box ->
+      t.click_state
+      <- Click_state.Create_object_select_final
+           (Objects.Box, first_selected_pos)
+    | Objects.Line ->
+      t.click_state
+      <- Click_state.Create_object_select_final
+           (Objects.Ball, first_selected_pos)
+    | Objects.Cup ->
+      t.click_state
+      <- Click_state.Create_object_select_final
+           (Objects.Ball, first_selected_pos))
+;;
+
+let handle_select_object_final (t : World.t) obj first_selected_pos x y =
+  let second_selected_pos =
+    { Vector.x = Float.of_int x; y = Float.of_int y }
+  in
+  let dist = Vector.dist first_selected_pos second_selected_pos in
+  if Interface.Canvas.in_bounds t.ui.canvas x y
+  then (
+    match obj with
+    | Objects.Ball ->
       let new_ball =
-        Objects.Ball.create ~center:create_at ~mass:10.0 ~radius:30.0
+        Objects.Ball.create ~center:first_selected_pos ~mass:10.0 ~radius:dist
       in
       Interface.Canvas.add_ball t.ui.canvas new_ball
-    | Cup ->
-      let new_cup = Objects.Cup.create ~min:create_at ~max:create_at_max in
+    | Objects.Cup ->
+      let new_cup = Objects.Cup.create ~min:first_selected_pos ~max:second_selected_pos in
       Interface.Canvas.add_cup t.ui.canvas new_cup
-    | Box ->
+    | Objects.Box ->
       let new_box =
-        Objects.Box.create ~min:create_at ~max:create_at_max ~mass:10.0
+        Objects.Box.create ~min:first_selected_pos ~max:second_selected_pos ~mass:10.0
       in
       Interface.Canvas.add_box t.ui.canvas new_box
-    | Line ->
+    | Objects.Line ->
       let new_line =
-        Objects.Line.create ~first_endp:create_at ~second_endp:create_at_max
+        Objects.Line.create ~first_endp:first_selected_pos ~second_endp:second_selected_pos
       in
       Interface.Canvas.add_line t.ui.canvas new_line)
 ;;
@@ -69,11 +96,15 @@ let rec handle_click (t : World.t) : unit Deferred.t =
     let x = event.mouse_x in
     let y = event.mouse_y in
     (match t.click_state with
-     | Click_state.Create_object_now obj -> handle_create_object t obj x y
+     | Click_state.Create_object_select_first obj ->
+       handle_select_object_first t obj x y
+     | Click_state.Create_object_select_final (obj, first_selected_pos) ->
+       handle_select_object_final t obj first_selected_pos x y
      | Click_state.Drag_current_object obj -> handle_drag_object t obj x y
      | Click_state.Select_current_object obj ->
        handle_select_object t obj x y
-     | Click_state.Free_state -> handle_free_state t x y);
+     | Click_state.Free_state -> handle_free_state t x y
+     | _ -> ());
     handle_click t)
   else handle_click t
 ;;
--- lib/click_state.mli
+++ lib/click_state.mli
@@ -2,6 +2,8 @@ open! Core
 
 type t =
   | Create_object_now of Objects.t
+  | Create_object_select_first of Objects.t
+  | Create_object_select_final of (Objects.t * Vector.t)
   | Drag_current_object of Objects.t
   | Select_current_object of Objects.t
   | Free_state
